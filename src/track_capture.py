8bc1ee3f05b3df388465cf567394fbf1c4179174
Joao Nascimento <joao.nascimento@testrigor.com>
2025-12-07 16:50:53 +0100

    track capture translated from matlab

---
+++ src/track_capture.py
@@ -0,0 +1,26 @@
+from constants import W, psiW
+from numpy import atan2, cos, asin, sin, pi
+
+def get_x_dot(V, gamma, psi, wind, psi_wind):
+    return V * cos(gamma) * cos(psi) + wind * cos(psi_wind + pi);
+
+def get_y_dot(V, gamma, psi, wind, psi_wind):
+    return V * cos(gamma) * sin(psi) + wind * sin(psi_wind + pi);
+
+def get_drift(V, gamma, wind, psi_wind, track):
+    arg = wind / (V*cos(gamma)) * sin(pi + psi_wind - track);
+    arg = max(-1, min(1, arg))
+    return asin(arg)
+
+# Named as 'capture_de_route' in MATLAB
+def track_capture(V, gamma, psi):
+    x_dot = get_x_dot(V, gamma, psi, W, psiW)
+    y_dot = get_y_dot(V, gamma, psi, W, psiW)
+    
+    track = atan2(y_dot, x_dot)
+    drift = get_drift(V, gamma, W, psiW, track)
+    return {
+		"x_dot": x_dot,
+		"y_dot": y_dot,
+		"drift": drift
+	}
